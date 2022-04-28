53d78e0d168dffc01f8290a623c8217c7f706b57
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-04-29 01:51:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,267 @@
+Amplitude.init({
+  songs: [
+    {
+      name: "Black & Blue",
+      artist: "Avicii feat. Aloe Blacc, Mac Davis",
+      album: "Single",
+      url: "./music/track/Black & Blue.mp3",
+      cover_art_url: "./music/cover/Black & Blue.jpg",
+    },
+    {
+      name: "Bright Lights (Version 1)",
+      artist: "Avicii, Syn Cole",
+      album: "Single",
+      url: "./music/track/Bright Lights (Version 1).mp3",
+      cover_art_url: "./music/cover/Bright Lights (Version 1).jpg",
+    },
+    {
+      name: "Bright Lights (Version 2)",
+      artist: "Avicii, Syn Cole",
+      album: "Single",
+      url: "./music/track/Bright Lights (Version 2).mp3",
+      cover_art_url: "./music/cover/Bright Lights (Version 1).jpg",
+    },
+    {
+      name: "Broken Heart & Hennessy",
+      artist: "Avicii feat. Mike Posner",
+      album: "Single",
+      url: "./music/track/Broken Heart & Hennessy.mp3",
+      cover_art_url: "./music/cover/Broken Heart & Hennessy.jpg",
+    },
+    {
+      name: "Can't Get Enough",
+      artist: "Avicii, Cazzette feat. The High",
+      album: "Single",
+      url: "./music/track/Can't Get Enough.mp3",
+      cover_art_url: "./music/cover/Can't Get Enough.jpg",
+    },
+    {
+      name: "Can't Love You Again",
+      artist: "Avicii, Tom Odell",
+      album: "Single",
+      url: "./music/track/Can't Love You Again.mp3",
+      cover_art_url: "./music/cover/Can't Love You Again.jpg",
+    },
+    {
+      name: "Chained",
+      artist: "Avicii, AlexST",
+      album: "Single",
+      url: "./music/track/Chained.mp3",
+      cover_art_url: "./music/cover/Chained.jpg",
+    },
+    {
+      name: "Deamons",
+      artist: "Avicii, Bonn",
+      album: "Single",
+      url: "./music/track/Deamons.mp3",
+      cover_art_url: "./music/cover/Deamons.jpg",
+    },
+    {
+      name: "Enough is Enough",
+      artist: "Avicii",
+      album: "Single",
+      url: "./music/track/Enough is Enough.mp3",
+      cover_art_url: "./music/cover/Enough is Enough.jpg",
+    },
+    {
+      name: "Faster Than Light",
+      artist: "Avicii feat. Sandro Cavazza",
+      album: "Single",
+      url: "./music/track/Faster Than Light.mp3",
+      cover_art_url: "./music/cover/Faster Than Light.jpg",
+    },
+    {
+      name: "Finally",
+      artist: "Avicii, Kings Of Tomorrow Kings Of Tomorrow feat. Julie McKnight",
+      album: "Single",
+      url: "./music/track/Finally.mp3",
+      cover_art_url: "./music/cover/Finally.jpg",
+    },
+    {
+      name: "Heaven",
+      artist: "Avicii feat. Simon Aldred",
+      album: "Single",
+      url: "./music/track/Heaven.mp3",
+      cover_art_url: "./music/cover/Heaven.jpg",
+    },
+    {
+      name: "How Many Lovers",
+      artist: "Avicii feat. Simon Aldred",
+      album: "Single",
+      url: "./music/track/How Many Lovers.mp3",
+      cover_art_url: "./music/cover/How Many Lovers.jpg",
+    },
+    {
+      name: "I'll be Gone",
+      artist: "Avicii feat. Joakim Berg",
+      album: "Single",
+      url: "./music/track/I'll be Gone.mp3",
+      cover_art_url: "./music/cover/I'll be Gone.jpg",
+    },
+    {
+      name: "In Your Arms",
+      artist: "Avicii, Frakz Room feat. Agnes, Vargas & Lagola",
+      album: "Single",
+      url: "./music/track/In Your Arms.mp3",
+      cover_art_url: "./music/cover/In Your Arms.jpg",
+    },
+    {
+      name: "Live Your Life",
+      artist: "Avicii, Afrojack, David Guetta, Ne-Yo",
+      album: "Single",
+      url: "./music/track/Live Your Life.mp3",
+      cover_art_url: "./music/cover/Live Your Life.jpg",
+    },
+    {
+      name: "Lord",
+      artist: "Avicii, Sandro Cavazza",
+      album: "Single",
+      url: "./music/track/Lord.mp3",
+      cover_art_url: "./music/cover/Lord.jpg",
+    },
+    {
+      name: "Make Your Move",
+      artist: "Avicii, Redroche, Dave Armstrong",
+      album: "Single",
+      url: "./music/track/Make Your Move.mp3",
+      cover_art_url: "./music/cover/Make Your Move.jpg",
+    },
+    {
+      name: "No Pleasing A Woman",
+      artist: "Avicii",
+      album: "Single",
+      url: "./music/track/No Pleasing A Woman.mp3",
+      cover_art_url: "./music/cover/No Pleasing A Woman.jpg",
+    },
+    {
+      name: "Our Love",
+      artist: "Avicii feat. Sandro Cavazza",
+      album: "Single",
+      url: "./music/track/Our Love.mp3",
+      cover_art_url: "./music/cover/Our Love.jpg",
+    },
+    {
+      name: "Revolver",
+      artist: "Avicii, Philter",
+      album: "Single",
+      url: "./music/track/Revolver.mp3",
+      cover_art_url: "./music/cover/Revolver.jpg",
+    },
+    {
+      name: "Rock Me",
+      artist: "Avicii, Mike Posner",
+      album: "Single",
+      url: "./music/track/Rock Me.mp3",
+      cover_art_url: "./music/cover/Rock Me.jpg",
+    },
+    {
+      name: "Silence",
+      artist: "Avicii",
+      album: "Single",
+      url: "./music/track/Silence.mp3",
+      cover_art_url: "./music/cover/Silence.jpg",
+    },
+    {
+      name: "Someone New",
+      artist: "Avicii, Triangle",
+      album: "Single",
+      url: "./music/track/Someone New.mp3",
+      cover_art_url: "./music/cover/Someone New.jpg",
+    },
+    {
+      name: "Somethings Got A Hold On Me vs Levels",
+      artist: "Avicii, Skrillex, Etta James",
+      album: "Single",
+      url: "./music/track/Somethings Got A Hold On Me vs Levels.mp3",
+      cover_art_url: "./music/cover/Somethings Got A Hold On Me vs Levels.jpg",
+    },
+    {
+      name: "Stepping Stone",
+      artist: "Avicii, AlexST",
+      album: "Single",
+      url: "./music/track/Stepping Stone.mp3",
+      cover_art_url: "./music/cover/Stepping Stone.jpg",
+    },
+    {
+      name: "Street Player",
+      artist: "Avicii, Chicago",
+      album: "Single",
+      url: "./music/track/Street Player.mp3",
+      cover_art_url: "./music/cover/Street Player.jpg",
+    },
+    {
+      name: "Sunshine vs Spectrum",
+      artist: "Avicii & David Guetta vs. Florence & Machine",
+      album: "Single",
+      url: "./music/track/Sunshine vs Spectrum.mp3",
+      cover_art_url: "./music/cover/Sunshine vs Spectrum.jpg",
+    },
+    {
+      name: "Take Me In Your Arms",
+      artist: "Avicii, The Isley Brothers",
+      album: "Single",
+      url: "./music/track/Take Me In Your Arms.mp3",
+      cover_art_url: "./music/cover/Take Me In Your Arms.jpg",
+    },
+    {
+      name: "T-Bone",
+      artist: "Avicii",
+      album: "Single",
+      url: "./music/track/T-Bone.mp3",
+      cover_art_url: "./music/cover/T-Bone.jpg",
+    },
+    {
+      name: "The Other Side",
+      artist: "Avicii feat. Cam",
+      album: "Single",
+      url: "./music/track/The Other Side.wav",
+      cover_art_url: "./music/cover/The Other Side.jpg",
+    },
+    {
+      name: "UMF 2013",
+      artist: "Avicii",
+      album: "Single",
+      url: "./music/track/UMF 2013.flac",
+      cover_art_url: "./music/cover/UMF 2013.jpg",
+    },
+    {
+      name: "Unbreakable",
+      artist: "Avicii, BUNT feat. Sandro Cavazza & Clarece Coffee Jr",
+      album: "Single",
+      url: "./music/track/Unbreakable.mp3",
+      cover_art_url: "./music/cover/Unbreakable.jpg",
+    },
+  ],
+});
+
+/*
+  Shows the playlist
+*/
+document
+  .getElementsByClassName("show-playlist")[0]
+  .addEventListener("click", function () {
+    document
+      .getElementById("white-player-playlist-container")
+      .classList.remove("slide-out-top");
+    document
+      .getElementById("white-player-playlist-container")
+      .classList.add("slide-in-top");
+    document.getElementById("white-player-playlist-container").style.display =
+      "block";
+  });
+
+/*
+  Hides the playlist
+*/
+document
+  .getElementsByClassName("close-playlist")[0]
+  .addEventListener("click", function () {
+    document
+      .getElementById("white-player-playlist-container")
+      .classList.remove("slide-in-top");
+    document
+      .getElementById("white-player-playlist-container")
+      .classList.add("slide-out-top");
+    document.getElementById("white-player-playlist-container").style.display =
+      "none";
+  });
